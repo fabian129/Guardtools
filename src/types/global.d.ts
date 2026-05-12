dbb855389147def63efa7c4f4cb1c21f8fac3ecc
Fabian Lindström <fabian@zaitex.net>
2026-05-12 03:47:43 +0200

    fix: React 19 JSX type declaration for iconify-icon

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,18 +1,22 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 
-declare namespace JSX {
-  interface IntrinsicElements {
-    'iconify-icon': React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement> & {
-        icon?: string;
-        width?: string | number;
-        height?: string | number;
-        inline?: boolean;
-        flip?: string;
-        rotate?: string | number;
-      },
-      HTMLElement
-    >;
+import 'react';
+
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements {
+      'iconify-icon': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement> & {
+          icon?: string;
+          width?: string | number;
+          height?: string | number;
+          inline?: boolean;
+          flip?: string;
+          rotate?: string | number;
+        },
+        HTMLElement
+      >;
+    }
   }
 }
 
